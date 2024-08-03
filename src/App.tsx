0f3fd2f724633b7fe609d9473e0d4698b9276856
cem20903 <cem20903@gmail.com>
2024-08-03 14:47:44 +0200

    Add Login View

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import Login from "./Views/Login/Login";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Login />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Views/Login/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+
+import "./login.css";
+
+function Login() {
+	const email = useRef<HTMLInputElement>(null);
+	const password = useRef<HTMLInputElement>(null);
+
+	function onSendLogin(event: React.FormEvent) {
+		event.preventDefault();
+	}
+
+	return (
+		<div className='login'>
+			<h2>Login</h2>
+			<div>
+				<form
+					onSubmit={onSendLogin}
+					className='login--form'
+				>
+					<label htmlFor='email-field'>Correo</label>
+					<input
+						id='email-field'
+						type='text'
+						name='email'
+						aria-label='email'
+						className='login--input'
+						ref={email}
+						placeholder='Introduzca su correo'
+						required
+					/>
+					<label htmlFor='password-field'>Password</label>
+					<input
+						id='password-field'
+						type='password'
+						name='password'
+						aria-label='password'
+						className='login--input'
+						placeholder='Introduzca su contraseña'
+						ref={password}
+						required
+					/>
+					<button
+						className='login--button'
+						type='submit'
+					>
+						Log In
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
