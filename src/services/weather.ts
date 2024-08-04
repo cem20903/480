825abb06e2e933b3c5a275c26cb39e2bdf707249
cem20903 <cem20903@gmail.com>
2024-08-04 18:32:31 +0200

    replace names

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -2,23 +2,18 @@ import { isToday, transformDateFromUnixTimestamp } from "../utils/dates"
 
 type Weather = [{ dt: number, dt_txt: string, weather: [{ icon: string }], main: { temp_max: number, temp_min: number, temp: number } }]
 
-
 function filterByDate(weatherList: Weather) {
-
   const weatherListWithFormatedDate = weatherList.map(weatherItem => {
-
     const currentDate = transformDateFromUnixTimestamp(weatherItem.dt)
-
     return {
       ...weatherItem,
       date: currentDate
     }
   })
-
   return weatherListWithFormatedDate.filter(weatherItem => isToday(weatherItem.date))
 }
 
-function formatWeatherList(weatherList: Weather) {
+function weatherListFormated(weatherList: Weather) {
 
   const currentWeather = filterByDate(weatherList)
 
@@ -38,12 +33,13 @@ function formatWeatherList(weatherList: Weather) {
 }
 
 
+const URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 function getWeather(city: string) {
-  return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_WEATHER_API}&mode=json`)
+  return fetch(`${URL}?q=${city}&appid=${process.env.REACT_APP_WEATHER_API}&mode=json`)
     .then((response) => response.json())
     .then((response) => {
-      return formatWeatherList(response.list)
+      return weatherListFormated(response.list)
     });
 }
 
