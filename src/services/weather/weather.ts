8cd9712c7ab718dea6279f181af483ea45905cf4
cem20903 <cem20903@gmail.com>
2024-08-07 11:54:15 +0200

    Add language type

--- src/services/weather/weather.ts
+++ src/services/weather/weather.ts
@@ -1,6 +1,7 @@
+import { language } from "../../types";
 import weatherRepository from "./weatherRepository";
 
-function getWeatherByCity(city: string, lang: string) {
+function getWeatherByCity(city: string, lang: language | string) {
   return weatherRepository.fetchWeather(city, lang);
 }
 
